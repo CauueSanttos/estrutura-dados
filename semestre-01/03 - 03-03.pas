5daa773963d8f52ac290e712b0e2abfb447a2f65
Cauê <cauedossantossilva@hotmail.com>
2020-03-03 21:02:27 -0300

    feat: Criação do exercício três

---
+++ semestre-01/03 - 03-03.pas
@@ -0,0 +1,9 @@
+// Você foi contratado pela empresa robots corporation a qual necessita de um 
+// módulo de desenvolvimento para atendimentos, através de chatbots. Os requisitos são: 
+// Gerenciar uma fila de atendimento para novos clientes, 
+// uma fila para mudança de planos e uma terceira para reclamações.
+
+Program Pzim ;
+Begin
+  
+End.
